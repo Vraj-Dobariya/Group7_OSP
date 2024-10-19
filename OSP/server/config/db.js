57a1dabe5a6db3f0dfb8fc1f9d438d8ee3cc01c2
202201138 <202201138@daiict.ac.in>
2024-10-19 19:58:33 +0530

    Update db.js

--- OSP/server/config/db.js
+++ OSP/server/config/db.js
@@ -1,24 +1,38 @@
-const { config } = require("dotenv");
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
 
 const pool = new Pool({
  
-  
-    connectionString: process.env.DATABASE_URL,
+    // connectionString: process.env.DATABASE_URL,
+    //testDB
+    connectionString: "postgresql://ospdb_user:plPDKR20OhfDsB5wSEBOGilf9RKr7Xea@dpg-crts9d52ng1s73ceertg-a.singapore-postgres.render.com/ospdb",
     ssl: true, 
   });
 
 
+  const createTablesQuery = `
+CREATE TABLE USERS(
+  	id SERIAL PRIMARY KEY,
+  	username TEXT,
+  	email TEXT,
+  	password TEXT,
+  	pic TEXT
+);
 
-  pool
-  .connect()
-  .then(() => console.log("Connected to PostgreSQL"))
+`;
+
+pool
+  .query(createTablesQuery)
+  .then(() => {
+    console.log("Tables successfully created or already exist.");
+  })
   .catch((err) => {
-    console.error("Error connecting to PostgreSQL:", err.message);
-    process.exit(1); // Exit the application on connection error
+    console.error("Creating tables:", err.message);
   });
+
+
 
 
 
