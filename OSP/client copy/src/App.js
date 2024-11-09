9dee1ecd2c9e4f0d897a782a979274ccdd190445
202201166 <202201166@daiict.ac.in>
2024-11-09 18:34:46 +0530

    Update App.js

--- OSP/client copy/src/App.js
+++ OSP/client copy/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin/Admin';
 import AddScholarship from './components/Admin/AddScholarship'; // Import your AddScholarship page
+import Navbar from './components/Admin/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Admin />} /> {/* Admin (Dashboard) */}
         <Route path="/admin/add-scholarship" element={<AddScholarship />} /> {/* Add Scholarship page */}
