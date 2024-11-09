0a98f1bdb5a541df1e9522d9b20fa14e8d83748e
kashvi710 <202201149@daiict.ac.in>
2024-11-09 17:30:21 +0530

    added admin side add scholarship option with CSS styling

--- OSP/client copy/src/App.js
+++ OSP/client copy/src/App.js
@@ -1,18 +1,18 @@
 // App.jsx
 import './App.css';
-import LoginRegister from './components/LoginRegister/LoginRegister';
-import ForgotPassword from './components/LoginRegister/ForgotPassword';
 import { Route, Routes } from 'react-router-dom';
+import Admin from './components/Admin/Admin';
+import AddScholarship from './components/Admin/AddScholarship'; // Import your AddScholarship page
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LoginRegister />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Admin />} /> {/* Admin (Dashboard) */}
+        <Route path="/admin/add-scholarship" element={<AddScholarship />} /> {/* Add Scholarship page */}
       </Routes>
     </div>
   );
 }
 
-export default App;
+export default App;
