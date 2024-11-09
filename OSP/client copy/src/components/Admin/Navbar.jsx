73a9ffa755d9d542d08192b1a9c818808d1a7f52
202201166 <202201166@daiict.ac.in>
2024-11-09 16:28:33 +0530

    Dashboard for admin page
    Added router links to the admin page.

--- OSP/client copy/src/components/Admin/Navbar.jsx
+++ OSP/client copy/src/components/Admin/Navbar.jsx
@@ -1,40 +1,72 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+
 const Navbar = () => {
-    return ( 
-        <>
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Dashboard</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Add Scholarship</a>
-                        </li>
-                        
-                       
-                    </ul>
-                    <form className="d-flex" role="search">
-                        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
-                        <button type="button" className="btn btn-primary">Logout</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
-        <div className="jumbotron">
-            <br /><br /><br />
-            <br /><br /><br />
-  <h1 className="display-4">Welcome back Admin!</h1>
-  <p className="lead"></p>
-  {/* <hr className="my-4"/> */}
-  {/* <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> */}
-  {/* <a className="btn btn-primary btn-lg" href="#" role="button">Manage Scholarship</a> */}
-</div>
+  return ( 
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            Dashboard
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/admin/add-scholarship" className="nav-link active" aria-current="page">
+                  Add Scholarship
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+            {/* Profile Icon Dropdown */}
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FaUserCircle size={24} /> {/* Profile icon */}
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <Link className="dropdown-item" to="/admin/profile">View Profile</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/admin/logout">Logout</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-     );
-}
- 
+  );
+};
+
 export default Navbar;
