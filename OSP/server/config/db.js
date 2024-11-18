5275d6e1a818d690d68797b978722cf56a93b8f6
202201166 <202201166@daiict.ac.in>
2024-11-18 16:59:57 +0530

    Added Admin Profile page, Added Backend for View Applicants and View Applications (#26)
    Faced a lot of challenges in the AdminProfile page, as in our database schema email is a primary key, in table users. so we cannot update in easily. So finally we decided to keep the email to be immutable. a
    
    Added backend code for View Application , to fetch the list of scholarships from the database , and backend code for View applicants.
    
    Updated the App.js files as it had a few discrepancies.
    Updated the Navbar.jsx file.
    Updated ListofScholarships.jsx
    Updated ViewApplicant.jsx file
    Updated Userroute.js, Scholarshiproutes.js

--- OSP/server/config/db.js
+++ OSP/server/config/db.js
@@ -4,9 +4,6 @@ require("dotenv").config();
 const pool = new Pool({
   // connectionString: process.env.DATABASE_URL,
   //testDB
-  // connectionString: "postgresql://ospdb_user:plPDKR20OhfDsB5wSEBOGilf9RKr7Xea@dpg-crts9d52ng1s73ceertg-a.singapore-postgres.render.com/ospdb",
-  // ssl: true,
-
   host: "10.100.71.21",
   port: 5432,
   user: "202201106",
