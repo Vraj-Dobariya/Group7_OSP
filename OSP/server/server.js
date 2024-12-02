afad0e02708db5fa2b110ebd72ec80398544e60c
Vraj dobariya <202201106@daiict.ac.in>
2024-12-02 22:23:47 +0530

    Update server.js2

--- OSP/server/server.js
+++ OSP/server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://group7-osp.vercel.app/",
+    origin: "https://group7-osp.vercel.app",
   })
 );
 
